f319399f2640f0d07ef897b1f0e4d71f9c00d338
A-Bin1 <binamalexandra@gmail.com>
2023-05-16 19:57:32 -0700

    GetRedditStockData API Program

--- API_Finance/GetRedditStockData.py
+++ API_Finance/GetRedditStockData.py
@@ -7,23 +7,26 @@
 #API url = 'https://tradestie.com/api/v1/apps/reddit'
 
 import pandas
-import numpy
 import requests
 import datetime
+import argparse
 
-#adds amount of days to start date to compute a range
-def add_date_range(d, n):
-    date_range = [d + datetime.timedelta(days=i) for i in range(n)]
-    return(date_range)
+parser = argparse.ArgumentParser(description = "Script will generate a csv file of Stock Data from Reddit", formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument("--Number", "-N", help = "Number of days to be generated for Date Range", required = True, type=int)
+parser.add_argument("--StartDate", "-D", help = "Start Date of Range", required = True)
+parser.add_argument("--Output", "-O", help = "Name of output file. CSV format", required = True)
+args = parser.parse_args()
 
-#retrieves Top 50 Stocks Data per Day from add_date_range function
-#creates one df as csv export
-def get_redditAPI_data(datelist):
+
+def get_redditAPI_data():
+    startDateV = datetime.datetime.strptime(args.StartDate, '%Y-%m-%d')
+    datelist = ([startDateV + datetime.timedelta(days=i) for i in range(args.Number)])
+    datelistf = ([d.strftime('%Y-%m-%d') for d in datelist])
     requestl = []
     reddit_data = []
     reddit_data_dfl = []
     urlpart = 'https://tradestie.com/api/v1/apps/reddit?date='
-    for dt in datelist:
+    for dt in datelistf:
         url = requests.get(urlpart + dt)
         requestl.append(url)
     for u in requestl:
@@ -33,24 +36,18 @@ def get_redditAPI_data(datelist):
         reddit_dfp = pandas.DataFrame(i)
         reddit_data_dfl.append(reddit_dfp)
     reddit_df = pandas.concat(reddit_data_dfl)
-    datelistn = [datetime.datetime.strptime(datelist[i], '%Y-%m-%d') for i in range(len(datelist))]
+    datelistn = [datetime.datetime.strptime(datelistf[i], '%Y-%m-%d') for i in range(len(datelistf))]
     datelistnm = datelistn * int(len(reddit_df)/len(datelistn))
     datelistnm_s = sorted(datelistnm)
     
     reddit_df['ID'] = reddit_df.index +1
     reddit_df["Date"] = datelistnm_s
-    print(reddit_df.head(n=10))
-    reddit_df.to_csv('reddit_df.csv', sep='|')
+    reddit_df.to_csv(args.Output, sep=',')
+    return
 
 
-dt_inpt = input("Enter date YYYY-MM-DD")
-dt_inptf = datetime.datetime.strptime(dt_inpt, '%Y-%m-%d')
-int_inpt = int(input("How many days of data?"))
-date_range = add_date_range(dt_inptf,int_inpt)
-date_rangef = []
-for single_date in date_range:
-     s = single_date.strftime("%Y-%m-%d")
-     date_rangef.append(s)
+if __name__ == '__main__':
+    get_redditAPI_data()
 
-get_redditAPI_data(date_rangef)
+
 
